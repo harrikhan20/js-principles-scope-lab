95f69d3b7fbafa95535d47a372ac639e171a3ae4
harris <khanharris7@yahoo.com>
2021-05-26 00:41:50 -0500

    commit tree db8a0c805b4f92899d9c8c8a1ac6cf19c5fff4e5

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution in this file!
+// Write your solution in this fil
+var customerName = 'bob';
+
+
+console.log(customerName);
+
+
+
+    //customerName = 'bob'
+
+  //customerNameU = customerName.toUpperCase()
+
+ //console.log(customerName.toUpperCase());
+ function upperCaseCustomerName(customerName){
+ 
+   
+  
+   
+   return customerName.toUpperCase();
+  //bestCustomer = 'not ' + customerName;
+     
+  }
+   //return customerNameU;
+
+
+//upperCaseCustomerName();
+
+//window.customerName.toUpperCase();
+
+//console.log(window.customerName.toUpperCase());
+
+
+
+/*function setBestCustomer(){
+ 
+ window.bestCustomer = 'not bob';
+ return bestCustomer;
+//bestCustomer = 'not ' + customerName;
+   
+}
+setBestCustomer();
+window.bestCustomer = 'not bob';
+
+
+
+function overwriteBestCustomer(){
+overwriteBestCustomer = 'maybe bob';
+window.bestCustomer = 'maybe bob';
+return bestCustomer;    
+}*/
+
